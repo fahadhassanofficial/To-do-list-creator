88c02e10dd5bd44554ba115ca1190beb46096832
fahadhassanofficial <fahad.hassan@consultant.com>
2024-04-16 17:54:00 +0500

    successfully completed

---
+++ todo.ts
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+let todo = [];
+let condition = true;
+
+while(condition)
+  {
+
+let addTask = await inquirer.prompt 
+(
+   [ 
+      {
+        name: "todo",
+        type: "input",
+        message: "What you want to add in your todo list?"
+
+      },
+    
+      {
+        name: "addmore",
+        type: "confirm",
+        message: "do you want to add more?",
+        default: "false"
+
+      }
+   ]
+);
+todo.push(addTask.todo);
+condition = addTask.addmore
+console.log(todo);
+
+}
